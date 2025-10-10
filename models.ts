369b6b2190bad75e4a8001d55d380d5f4d519ef7
iamakilelias <akilelias9@gmail.com>
2025-10-10 02:08:49 -0400

    working on the staff management module first

---
+++ models.ts
@@ -0,0 +1,24 @@
+const enum Gender {
+    Male = "male",
+    Female = "female"
+}
+
+const enum 
+
+
+class Employee {
+    constructor(
+        id: number,
+        //personal info
+        firstName: string, 
+        lastName: string,
+        gender: Gender,
+        dob: string,
+        age: number, 
+        parent: boolean,
+        maritalStatus: boolean,
+        //job info
+        position: string,
+
+    )
+}
