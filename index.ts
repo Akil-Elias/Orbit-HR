4cc8e18c3da7d7b62422481318b739715269e255
iamakilelias <akilelias9@gmail.com>
2025-10-12 22:46:17 -0400

    added bun

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("ts go!")
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+      return new Response("Bun!");
+    },
+  });
+  
+console.log(`Listening on http://localhost:${server.port} ...`);
+  
